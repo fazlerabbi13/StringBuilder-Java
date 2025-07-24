56880d1aae182a652f76c95caa0d34fb37104d7e
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-24 20:26:09 +0600

    insert a string specific index

---
+++ Insert.java
@@ -0,0 +1,9 @@
+public class Insert{
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Learning");
+
+         sb.insert(8, " Java");
+
+         System.out.println(sb);
+    }
+}
