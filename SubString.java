82ba53a5334abb98c365ae7345d61ccb58cfb8a2
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-24 20:46:41 +0600

    Get a substring from the StringBuilder

---
+++ SubString.java
@@ -0,0 +1,9 @@
+public class SubString {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+
+        String string = sb.substring(2,4);
+
+        System.out.println(string);
+    }
+}
