58dc1c1333e620174462212931fb03798e0353e1
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-24 20:40:39 +0600

    Get the current capacity of the StringBuilder

---
+++ Capacity.java
@@ -0,0 +1,9 @@
+public class Capacity {
+   public static void main(String[] args) {
+       StringBuilder sb = new StringBuilder("Hello");
+
+       int capacity=sb.capacity();
+
+       System.out.println(capacity);
+   } 
+}
