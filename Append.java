89d08e12fcfb667fa59457cb5c366008492efddf
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-24 19:51:10 +0600

    In Java, the append() method of StringBuilder class is used to add data to the end of an existing StringBuilder object.

---
+++ Append.java
@@ -0,0 +1,9 @@
+public class Append {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello World");
+
+        sb.append(", With Fazle");
+
+        System.out.println(sb);
+    }
+}
