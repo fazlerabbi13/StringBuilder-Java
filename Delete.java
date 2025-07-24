4656b2f0256e04a1fa608df061ca2800407ea7a0
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-24 20:35:18 +0600

    Delete a substring from the StringBuilder

---
+++ Delete.java
@@ -0,0 +1,7 @@
+public class Delete {
+    public static void main(String[] args) {
+        StringBuilder var1 = new StringBuilder("Learning");
+      var1.delete(1, 3);
+      System.out.println(var1);
+    }
+}
